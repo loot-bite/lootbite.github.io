349666deeac0c4d9810539ddc3b0675348f6bab8
loot-bite <lootbite1@gmail.com>
2026-09-16 21:54:56 -0500

    Change initial game time to 12 seconds
    Updated initial game time from 15 to 12 seconds.

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
-const INITIAL_TIME = 15;
+const INITIAL_TIME = 12;
 let timeLeft = INITIAL_TIME;
 let timerInterval;
 let gameRunning = false;
